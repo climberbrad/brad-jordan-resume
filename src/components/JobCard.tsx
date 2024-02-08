8bf5da9df359765dc1940cdcb27a9f748c194494
climberbrad <climberbrad@gmail.com>
2024-02-08 09:38:56 -0700

    moved job dates for mobile

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import {Box, CardContent, Chip, Divider, Typography} from "@mui/material";
+import {Box, CardContent, Chip, Divider, Grid, Typography} from "@mui/material";
 import {Job} from "./JobData.ts";
 
 export function JobCard({job}: { job: Job }): React.ReactElement {
     return (
         <CardContent sx={{transitionDelay: '20ms', '&:hover': {background: "#112538", border: 0, borderRadius: 2}}}>
-            <Box sx={{display: 'flex', gap: 2}}>
-                <Typography sx={{whiteSpace: 'noWrap'}} fontWeight='bolder' color='darkgrey' fontSize={14}>
-                    {job.startDate} - {job.endDate}
-                </Typography>
-                <Box>
+            <Grid container spacing={0}>
+                <Grid item md={2}>
+                    <Typography display={{xs: 'none', md: 'block'}} sx={{whiteSpace: 'noWrap'}} color='darkgrey' fontSize={14}>
+                        {job.startDate} - {job.endDate}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={10}>
                     <Typography fontFamily="'Quicksand', sans-serif" fontWeight='bolder' fontSize={18} gutterBottom>
                         {job.company}
+                    </Typography>
+                    <Typography display={{md: 'none'}} sx={{whiteSpace: 'noWrap'}} color='darkgrey' fontSize={14}>
+                        {job.startDate} - {job.endDate}
                     </Typography>
                     <Typography fontSize={16} gutterBottom>
                         {job.title}
@@ -24,10 +29,9 @@ export function JobCard({job}: { job: Job }): React.ReactElement {
                         {job.skills.map(skill => (
                             <Chip key={skill} sx={{background: '#344257', color: '#75e5d3'}} label={skill}/>
                         ))}
-
                     </Box>
-                </Box>
-            </Box>
+                </Grid>
+            </Grid>
         </CardContent>
     )
 }
