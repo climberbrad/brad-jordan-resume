1eae6f65852e024bf50c756c6bff3cc768c50d6f
climberbrad <climberbrad@gmail.com>
2024-02-14 10:42:14 -0700

    added WTP

--- src/components/ProjectData.ts
+++ src/components/ProjectData.ts
@@ -16,4 +16,13 @@ export const ProjectData: Project[] = [
             'You can reserve an accommodation and it appears in MyTrips. It was just fun to build' +
             'a simple reservation system and play around with some of the MUI components.'
     },
+    {
+        name: 'We the people',
+        link: 'https://we-the-people-oh5h.vercel.app/',
+        codeUrl: 'https://github.com/climberbrad/we-the-people/',
+        image: 'wtp.png',
+        description: 'I created a webapp to allow users to create seven day polls. ' +
+            'It is just a fun little app so my kids could create a poll to ask what movie they wanted to watch Friday nights. ' +
+            'The backend API is Node | Express | MongoDB (MERN stack) and the Frontend is React/TypeScript and MUI.'
+    }
 ]
